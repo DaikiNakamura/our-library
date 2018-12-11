92f98cd96343561202efa85abe92a066f3af9bce
中村大騎 <d_nakamura@waku-2.com>
2018-12-11 13:43:29 +0900

    書籍一覧

---
+++ components/BookList.vue
@@ -0,0 +1,45 @@
+<template>
+  <table class="table">
+    <!--見出し-->
+    <thead>
+    <tr>
+      <th>ISBN</th>
+      <th>タイトル</th>
+      <th>サブタイトル</th>
+      <th>著書</th>
+    </tr>
+    </thead>
+    <tfoot>
+    <tr>
+      <th>ISBN</th>
+      <th>タイトル</th>
+      <th>サブタイトル</th>
+      <th>著書</th>
+    </tr>
+    </tfoot>
+    <!--データ-->
+    <tbody>
+    <tr v-for="book in books" :key="book.isbn">
+      <td>{{ book.isbn }}</td>
+      <td>{{ book.title }}</td>
+      <td>{{ book.subTitle }}</td>
+      <td>{{ book.author }}</td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  export default {
+    name: "BookList",
+    computed: {
+      books() {
+        return this.$store.state.books.list;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,19 @@
       <button class="delete"></button>
       開発中！！
     </div>
+    <h3 class="title">書籍一覧</h3>
+    <book-list />
   </div>
 </template>
 
 <script>
+  import BookList from '~/components/BookList.vue'
+
+  export default {
+    components: {
+      BookList
+    }
+  }
 </script>
 
 <style>
