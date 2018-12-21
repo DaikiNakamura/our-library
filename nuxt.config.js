e67de22eae192863d82c509fbbd2f37b52da1a7b
中村大騎 <d_nakamura@waku-2.com>
2018-12-21 15:49:46 +0900

    meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'OUR LIBRARYで本の管理をしましょう！' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
